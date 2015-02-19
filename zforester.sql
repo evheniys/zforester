837fc0d74d8c9e920c75bd00b94c5a9a51557cfe
evheniys <evheniys@gmail.com>
2015-02-19 19:19:41 +0200

    19.01

--- zforester.sql
+++ zforester.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 16, 2015 at 08:01 AM
+-- Generation Time: Feb 19, 2015 at 07:13 PM
 -- Server version: 5.5.41-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.5
+-- PHP Version: 5.5.9-1ubuntu4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -33,7 +33,42 @@ CREATE TABLE IF NOT EXISTS `filelog` (
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `filelog`
+--
+
+INSERT INTO `filelog` (`id`, `userid`, `fileid`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, '2015-02-19 13:42:04', '2015-02-19 13:42:04'),
+(2, 1, 3, '2015-02-19 13:48:15', '2015-02-19 13:48:15');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `files`
+--
+
+CREATE TABLE IF NOT EXISTS `files` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `filename` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `mimetype` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `files`
+--
+
+INSERT INTO `files` (`id`, `filename`, `mimetype`, `description`, `created_at`, `updated_at`) VALUES
+(1, 'upload/adobe_pdf.pdf', 'multipart/octet-stream', 'Файл в формате Adobe PDF', '2015-02-19 07:34:50', '2015-02-19 07:34:50'),
+(2, 'upload/microsoft_word_doc.doc', 'application/msword', 'Файл в формате Microsoft Office DOC', '2015-02-19 07:35:18', '2015-02-19 07:35:18'),
+(3, 'upload/microsoft_word_docx.docx', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'Файл в формате Microsoft Office DOCX', '2015-02-19 07:35:45', '2015-02-19 07:35:45'),
+(4, 'upload/microsoft_excel_xls.xls', 'application/vnd.ms-excel', 'Файл в формате Microsoft Office XLS', '2015-02-19 07:36:01', '2015-02-19 07:36:01'),
+(5, 'upload/microsoft_excel_xlsx.xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'Файл в формате Microsoft Office XLSX', '2015-02-19 07:36:12', '2015-02-19 07:36:12');
 
 -- --------------------------------------------------------
 
@@ -53,9 +88,16 @@ CREATE TABLE IF NOT EXISTS `migrations` (
 INSERT INTO `migrations` (`migration`, `batch`) VALUES
 ('2014_10_12_000000_create_users_table', 1),
 ('2014_10_12_100000_create_password_resets_table', 1),
+('2015_02_15_154314_TurboSmsSend', 1),
+('2015_02_15_210512_CreateUserLog', 1),
+('2015_02_15_210529_CreateFileLog', 1),
+('2015_02_17_154908_Create_Files_table', 1),
+('2014_10_12_000000_create_users_table', 1),
+('2014_10_12_100000_create_password_resets_table', 1),
 ('2015_02_15_154314_TurboSmsSend', 2),
 ('2015_02_15_210512_CreateUserLog', 2),
-('2015_02_15_210529_CreateFileLog', 2);
+('2015_02_15_210529_CreateFileLog', 2),
+('2015_02_18_201317_ChangeFilesTable', 3);
 
 -- --------------------------------------------------------
 
@@ -105,7 +147,7 @@ CREATE TABLE IF NOT EXISTS `userlog` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `userid` int(11) NOT NULL,
   `activity` int(11) NOT NULL,
-  `ipaddress` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
+  `ipaddress` varchar(18) COLLATE utf8_unicode_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
@@ -141,7 +183,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `phonenumber`, `password`, `status`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, '+380958949336', '$2y$10$ThiYs.mY0wFUAsvKnUw8welBTO0tRDXC9HZNVkGRDL0WULDMr.OFK', 1, 'QViMS8Daz3HAk1WYb58SpCErrktY81dNuOfgzmORM2QuL6czKzE6sVFDqIet', '2015-02-15 21:47:59', '2015-02-15 21:48:54');
+(1, '+380958949336', '$2y$10$ThiYs.mY0wFUAsvKnUw8welBTO0tRDXC9HZNVkGRDL0WULDMr.OFK', 1, 'jKrmFHIvJQkQTIPU2OaVZH0lPmHnnX0NBm5NQbvpY4z1XGeRLau9n1R6S5A0', '2015-02-15 21:47:59', '2015-02-19 13:56:17');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
